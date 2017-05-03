a3bafc144d24d4cd8fa5a2681d9a7ed08b248d44
sathish kumar <vgsathish1995@gmail.com>
2017-05-03 13:54:20 +0530

    commit 6 -- Table view withNavigation controller

--- NotesApp/ViewController.swift
+++ NotesApp/ViewController.swift
@@ -8,8 +8,24 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    let list = ["Apple","Banana","Blue Berry","Grapes"]
+    
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        
+        return list.count
+    }
+    
 
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "cell")
+        cell.textLabel?.text = list[indexPath.row]
+        return (cell)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
